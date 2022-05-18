217fa0defb63ed19f0bf0f8e6428aed57a756717
SantiC <santicordoba93@gmail.com>
2022-05-18 19:58:06 -0300

    [UPDATE] agrego clase 7

---
+++ Clase7/Clase7/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] autos = { "BWM", "VOLVO", "FORD", "CHEVROLET", "PEUGEOT" };
+
+for (int i = 0; i < autos.Length; i++)
+{
+    Console.WriteLine($"Auto en la posión {i + 1} -> {autos[i]}");
+}
+
+string[] frutas = new string[4];
+
+frutas[0] = "Pera";
+frutas[1] = "Manzana";
+frutas[2] = "Naranja";
+frutas[3] = "Uvas";
+
+for (int i = 0; i < frutas.Length; i++)
+{
+    Console.WriteLine(frutas[i]);
+}
+
+int[] numeros = new int[5];
+
+Console.WriteLine("Ingrese 5 numeros");
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Ingrese un numero para la posicion {i}");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+int suma = 0;
+for(int i = 0; i < numeros.Length; i++)
+{
+    suma += numeros[i];
+}
+
+Console.WriteLine($"Suma de los numeros cargados: {suma}");
