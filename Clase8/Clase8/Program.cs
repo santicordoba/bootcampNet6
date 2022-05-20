aacc8651d67b08a38dc7b804852de87113e942bc
SantiC <santicordoba93@gmail.com>
2022-05-19 21:52:22 -0300

    Clase 8 + tarea 4

---
+++ Clase8/Clase8/Program.cs
@@ -0,0 +1,39 @@
+﻿//Matrices de dos dimensiones
+
+/*int filas = 5;
+int columnas = 2;
+
+int[,] notas = new int[filas, columnas];
+*/
+
+
+Console.WriteLine("Bienvenido a la carga de notas de los examenes: ");
+Console.WriteLine("Ingrese la cantidad de alumnos: ");
+int cantidadAlumnos=int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la cantidad de notas por alumno: ");
+int notasPorAlumno= int.Parse(Console.ReadLine());
+
+int[,] notas = new int[cantidadAlumnos,notasPorAlumno];
+
+// Cantidad de filas y de columnas
+int lengthFilas = notas.GetUpperBound(0) + 1;
+int lengthColumnas = notas.GetUpperBound(1) + 1;
+
+for (int i = 0; i < lengthColumnas; i++)
+{
+    Console.WriteLine($"Ingrese las notas del alumno nº= {i + 1}");
+    for (int j = 0; j < lengthFilas; j++)
+    {
+        Console.WriteLine($"Ingrese las nota nº= {j + 1}");
+        notas[j,i] = int.Parse(Console.ReadLine());
+    }
+}
+Console.WriteLine("A continuacion se imprimiran las notas de los alumnos en pantalla");
+for (int i = 0; i < lengthColumnas; i++)
+{
+    Console.WriteLine($"Notas del alumno nº= {i + 1}");
+    for (int j = 0; j < lengthFilas; j++)
+    {
+        Console.WriteLine($"Nota: {notas[j,i]}");
+    }
+}
---
+++ Tarea4/Tarea4/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Ejercicio
+
+Crear un programa que cumpla con los siguientes pasos
+
+1) Crear una matriz de dos dimensiones de tipo int llamada numeros,
+2) Determinar el tamaño de cada dimansion (fila, columnas) mediante valores ingresados por pantalla
+3) Declarar un vector de tipo double llamado promedios
+4) Recorrer la matriz para su carga con elementos de tipo int
+5) Recorrer la matriz para mostrar cada valor de la matriz
+6) Calcular el promedio de cada columna y asignarlo a la posicion correspondiente dentro del vector promedios
+7) Mostrar los promedios recorriendo el vector promedios*/
+
+Console.WriteLine("Ingrese la cantidad de columnas.");
+int columnas = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la cantidad de filas.");
+int filas = int.Parse(Console.ReadLine());
+
+int[,] numeros = new int[filas, columnas];
+
+int lengthFilas = numeros.GetUpperBound(0) + 1;
+int lengthColumnas = numeros.GetUpperBound(1) + 1;
+
+Console.WriteLine("_______________________________________");
+
+for (int i = 0; i < lengthFilas; i++)
+{
+    Console.WriteLine($"Se caragara la fila {i}");
+    for(int j = 0; j < lengthColumnas; j++)
+    {
+        Console.WriteLine($"Ingrese un numero para la posición {j}");
+        numeros[i,j] = int.Parse(Console.ReadLine());
+    }
+    Console.WriteLine("_______________________________________");
+}
+
+
+// VECTOR PROMEDIO DE MISMO TAMAÑO QUE LA CANTIDAD DE COLUMNAS
+double[] promedios = new double[filas];
+
+for (int i = 0; i < lengthFilas; i++)
+{
+    double suma = 0d;
+    Console.WriteLine($"Se imprimira en pantalla los numeros de la fila {i}");
+    for (int j = 0; j < lengthColumnas; j++)
+    {
+        Console.WriteLine($"Nº de la posición {j} -> {numeros[i,j]}");
+        suma += numeros[i, j];
+    }
+    Console.WriteLine("_______________________________________");
+    promedios[i] = suma / columnas;
+}
+
+
+for(int k = 0; k < promedios.Length; k++)
+{
+    Console.Write($"Promedios para la fila {k} -> ");
+    Console.WriteLine(promedios[k]);
+}
