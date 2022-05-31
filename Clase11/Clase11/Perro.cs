3ef9e9b59ab4a62e8d63c6ec6afe6243df70fa19
SantiC <santicordoba93@gmail.com>
2022-05-31 20:37:40 -0300

    clase 11

---
+++ Clase11/Clase11/Perro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase11
+{
+    public class Perro
+    {
+        public string Raza;
+        public string Tamanio;
+        public int Edad;
+        public string Color;
+
+        public void Ladrar()
+        {
+            Console.WriteLine("¡GUAU!");
+        }
+    }
+}
---
+++ Clase11/Clase11/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Clase11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instancia 1
+            Perro firulais = new Perro();
+            firulais.Color = "Negro";
+            firulais.Edad = 4;
+            firulais.Tamanio = "Grande";
+            firulais.Raza = "Mastin Napolitano";
+
+            // Instancia 2
+            Perro fatiga = new Perro();
+            fatiga.Color = "Blanco";
+            fatiga.Edad = 2;
+            fatiga.Tamanio = "Chico";
+            fatiga.Raza = "Maltes";
+
+            // Instancia 2
+            Perro cartucho = new Perro();
+            cartucho.Color = "Marron";
+            cartucho.Edad = 3;
+            cartucho.Tamanio = "Medio";
+            cartucho.Raza = "ChowChow";
+
+            // Lista de perros
+            var perros = new List<Perro>();
+            perros.Add(firulais);
+            perros.Add(fatiga);
+            perros.Add(cartucho);
+
+            /*Console.WriteLine(firulais.Raza);
+            Console.WriteLine(fatiga.Raza);
+            Console.WriteLine(cartucho.Raza);*/
+
+            foreach(var perro in perros)
+            {
+                Console.WriteLine(perro.Raza);
+                perro.Ladrar();
+            }
+        }
+    }
+}
