56c40e5b5efed5f189af6ebe8ced0e8c75085409
SantiC <santicordoba93@gmail.com>
2022-06-04 17:49:31 -0300

    clase 12

---
+++ Clase12/Clase12/Alumno.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase12
+{
+    internal class Alumno
+    {
+        public int Legajo { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string DNI { get; set; }
+        public int MyProperty { get; set; }
+        public List<int> Calificaciones { get; set; }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"{this.Nombre}, {this.Apellido}");
+        }
+
+        public void CargarCalificacion(int Nota, DateTime Fecha)
+        {
+            this.Calificaciones.Add(Nota);
+            Console.WriteLine(Fecha.ToString("dddd dd-MM-yy"));
+        }
+
+        public void CargarCalificacion(int nota)
+        {
+            this.Calificaciones.Add(nota);
+            var Fecha = DateTime.Now;
+            Console.WriteLine(Fecha.ToString("dddd dd-MM-yy"));
+        }
+
+        public decimal ObtenerPromedio()
+        {
+            int acumulador = 0;
+            foreach(var nota in Calificaciones)
+            {
+                acumulador += nota;
+            }
+            var promedio = acumulador / this.Calificaciones.Count;
+            return promedio;
+        }
+    }
+}
---
+++ Clase12/Clase12/Program.cs
@@ -0,0 +1,31 @@
+﻿using Clase12;  
+
+var alumno1 = new Alumno();
+
+
+alumno1.Apellido = "Córdoba";
+alumno1.Nombre = "Santiago";
+alumno1.Legajo = 11125;
+alumno1.DNI = "37357015";
+alumno1.Calificaciones = new List<int>();
+
+var alumno2 = new Alumno()
+{
+    Apellido = "Marconi",
+    Nombre = "Melina",
+    Legajo = 12345,
+    DNI = "38706685",
+    FechaNacimiento = new DateTime(1995, 1, 31),
+    Calificaciones = new List<int>()
+};
+
+alumno1.GetInfo();
+alumno2.GetInfo();
+
+alumno1.CargarCalificacion(4, DateTime.Now);
+alumno2.CargarCalificacion(5);
+alumno1.CargarCalificacion(8, DateTime.Now);
+
+Console.WriteLine(alumno1.Calificaciones.Count);
+
+Console.WriteLine($"Promedio del alumno {alumno1.Nombre} es {alumno1.ObtenerPromedio()}");
