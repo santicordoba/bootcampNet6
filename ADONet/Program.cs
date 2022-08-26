e0bbe753ef2e7444b9f5f43d297e97f65a8f9e7e
SantiC <santicordoba93@gmail.com>
2022-08-25 21:26:06 -0300

    Intro a BD

---
+++ ADONet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.SqlClient;
+
+var connectionString = @"Server=.\SQLEXPRESS;Database=Northwind;Trusted_Connection=yes;";
+
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+    connection.Open();
+    var command = new SqlCommand("SELECT * FROM Customers", connection);
+    using (var reader = command.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            Console.WriteLine($"{reader["CustomerID"]} - {reader["CompanyName"]} - {reader["ContactName"]}");
+        }
+    }
+
+    command = new SqlCommand("SELECT * FROM Suppliers", connection);
+    using (var reader = command.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            Console.WriteLine($"{reader["SupplierID"]} - {reader["CompanyName"]} - {reader["ContactName"]}");
+        }
+    }
+
+
+}
---
+++ EF-Database-First/Program.cs
@@ -0,0 +1,26 @@
+﻿using EF_Database_First.Models;
+using EFDatabaseFirst.Namespace;
+
+var ctx = new NorthwindContext();
+
+var categories =  ctx.Categories;
+
+foreach (var category in categories)
+{
+    Console.WriteLine($"Categoria: {category.CategoryName}");
+}
+
+var nuevaCategoria = new Category
+{
+    CategoryName = "Nueva Categoria",
+    Description = "Sarasa"
+};
+
+categories.Add(nuevaCategoria);
+
+var categories2 = ctx.Categories;
+
+foreach (var category in categories2)
+{
+    Console.WriteLine($"Categoria: {category.CategoryName}");
+}
